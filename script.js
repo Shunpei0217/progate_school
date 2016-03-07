3af05c5f7e2a53bb6bc4c9e55023a3c9e34fcfe6
Shunpei Tanzawa <shunpei@Shunpei-no-MacBook-Pro.local>
2016-03-07 15:17:51 +0900

    seventh commit

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(function() {
+
+	$('#read-more').click(function() {
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active').text('もっと詳しく');
+			$('#detail-description').fadeOut();
+		} else {
+			$(this).addClass('active').text('閉じる');
+			$('#detail-description').fadeIn();
+		};
+	});
+
 	$('.languages-box').hover(
 		function() {
-			$(this).find('p').fadeIn();
+			$(this).find('p').fadeIn(500);
 		},
 		function() {
-			$(this).find('p').fadeOut();
+			$(this).find('p').fadeOut(500);
 		});
+
+	$('.column-image').hover(function() {
+		$(this).addClass('zoom');
+		$(this).children('.zoom-black').fadeIn(300);
+	},
+	function() {
+		$(this).removeClass('zoom');
+		$(this).children('.zoom-black').fadeOut(300);
+	});
 });
